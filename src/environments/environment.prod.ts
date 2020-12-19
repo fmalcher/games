067fd7ef7a6194b81ad785b3fdd757ca5b015e88
Ferdinand Malcher <ferdinand@malcher.media>
2020-12-19 20:32:33 +0100

    add firebase credentials and modules

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBvlUQpgBicDHXp8BboAly7XRxrboSlObg',
+    authDomain: 'games-88897.firebaseapp.com',
+    projectId: 'games-88897',
+    storageBucket: 'games-88897.appspot.com',
+    messagingSenderId: '713999873976',
+    appId: '1:713999873976:web:dd409414b31ae1ca80014d',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBvlUQpgBicDHXp8BboAly7XRxrboSlObg',
+    authDomain: 'games-88897.firebaseapp.com',
+    projectId: 'games-88897',
+    storageBucket: 'games-88897.appspot.com',
+    messagingSenderId: '713999873976',
+    appId: '1:713999873976:web:dd409414b31ae1ca80014d',
+  },
 };
 
 /*
