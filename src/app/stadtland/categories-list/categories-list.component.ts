62fe7c4a91b66060e1594b0185b41c9d598f2b20
Ferdinand Malcher <ferdinand@malcher.media>
2020-12-23 16:51:20 +0100

    feat: disable newRound button when no categories are defined
    also some refactoring of the categories list component
    
    close #17

--- src/app/stadtland/categories-list/categories-list.component.ts
+++ src/app/stadtland/categories-list/categories-list.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { StadtlandService } from '../shared/stadtland.service';
-
+import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-categories-list',
   templateUrl: './categories-list.component.html',
   styleUrls: ['./categories-list.component.scss'],
 })
 export class CategoriesListComponent implements OnInit {
-  categories$ = this.sls.categories$;
+  @Input() categories: string[] = [];
 
-  constructor(private sls: StadtlandService) {}
+  constructor() {}
 
   ngOnInit(): void {}
 }
